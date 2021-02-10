8b037e62384f94c7b0c5ae5827d21e1fa973ad78
sebastien-riou <sriou@nimp.co.uk>
2021-02-10 15:46:24 +0100

    corenc soc

---
+++ src/main/scala/vexriscv/demo/Corenc.scala
@@ -0,0 +1,310 @@
+package vexriscv.demo
+
+
+import vexriscv.plugin._
+import vexriscv._
+import vexriscv.ip.{DataCacheConfig, InstructionCacheConfig}
+import spinal.core._
+import spinal.lib._
+import spinal.lib.bus.amba3.apb._
+import spinal.lib.bus.amba4.axi._
+import spinal.lib.com.jtag.Jtag
+import spinal.lib.com.uart.{Apb3UartCtrl, Uart, UartCtrlGenerics, UartCtrlMemoryMappedConfig}
+import spinal.lib.graphic.RgbConfig
+import spinal.lib.graphic.vga.{Axi4VgaCtrl, Axi4VgaCtrlGenerics, Vga}
+import spinal.lib.io.TriStateArray
+import spinal.lib.memory.sdram._
+import spinal.lib.misc.HexTools
+import spinal.lib.soc.pinsec.{PinsecTimerCtrl, PinsecTimerCtrlExternal}
+import spinal.lib.system.debugger.{JtagAxi4SharedDebugger, JtagBridge, SystemDebugger, SystemDebuggerConfig}
+
+import scala.collection.mutable.ArrayBuffer
+
+
+case class CorencConfig(axiFrequency : HertzNumber,
+                       onChipRamSize : BigInt,
+                       cpuPlugins : ArrayBuffer[Plugin[VexRiscv]],
+                       uartCtrlConfig : UartCtrlMemoryMappedConfig)
+
+object CorencConfig{
+
+  def default = {
+    val config = CorencConfig(
+      axiFrequency = 12 MHz,
+      onChipRamSize  = 32 kB,
+      uartCtrlConfig = UartCtrlMemoryMappedConfig(
+        uartCtrlConfig = UartCtrlGenerics(
+          dataWidthMax      = 8,
+          clockDividerWidth = 20,
+          preSamplingSize   = 1,
+          samplingSize      = 5,
+          postSamplingSize  = 2
+        ),
+        txFifoDepth = 16,
+        rxFifoDepth = 16
+      ),
+      cpuPlugins = ArrayBuffer(
+        new PcManagerSimplePlugin(0x80000000l, false),
+        new IBusCachedPlugin(
+          resetVector = 0x80000000l,
+          prediction = STATIC,
+          config = InstructionCacheConfig(
+            cacheSize = 4096,
+            bytePerLine =32,
+            wayCount = 1,
+            addressWidth = 32,
+            cpuDataWidth = 32,
+            memDataWidth = 32,
+            catchIllegalAccess = true,
+            catchAccessFault = true,
+            asyncTagMemory = false,
+            twoCycleRam = true,
+            twoCycleCache = true
+          )
+        ),
+        new DBusCachedPlugin(
+          config = new DataCacheConfig(
+            cacheSize         = 4096,
+            bytePerLine       = 32,
+            wayCount          = 1,
+            addressWidth      = 32,
+            cpuDataWidth      = 32,
+            memDataWidth      = 32,
+            catchAccessError  = true,
+            catchIllegal      = true,
+            catchUnaligned    = true
+          ),
+          memoryTranslatorPortConfig = null
+        ),
+        new StaticMemoryTranslatorPlugin(
+          ioRange      = _(31 downto 28) === 0xF
+        ),
+        new DecoderSimplePlugin(
+          catchIllegalInstruction = true
+        ),
+        new RegFilePlugin(
+          regFileReadyKind = plugin.SYNC,
+          zeroBoot = false
+        ),
+        new IntAluPlugin,
+        new SrcPlugin(
+          separatedAddSub = false,
+          executeInsertion = true
+        ),
+        new FullBarrelShifterPlugin,
+        new MulPlugin,
+        new DivPlugin,
+        new HazardSimplePlugin(
+          bypassExecute           = true,
+          bypassMemory            = true,
+          bypassWriteBack         = true,
+          bypassWriteBackBuffer   = true,
+          pessimisticUseSrc       = false,
+          pessimisticWriteRegFile = false,
+          pessimisticAddressMatch = false
+        ),
+        new BranchPlugin(
+          earlyBranch = false,
+          catchAddressMisaligned = true
+        ),
+        new CsrPlugin(
+          config = CsrPluginConfig(
+            catchIllegalAccess = false,
+            mvendorid      = null,
+            marchid        = null,
+            mimpid         = null,
+            mhartid        = null,
+            misaExtensionsInit = 66,
+            misaAccess     = CsrAccess.NONE,
+            mtvecAccess    = CsrAccess.NONE,
+            mtvecInit      = 0x80000020l,
+            mepcAccess     = CsrAccess.READ_WRITE,
+            mscratchGen    = false,
+            mcauseAccess   = CsrAccess.READ_ONLY,
+            mbadaddrAccess = CsrAccess.READ_ONLY,
+            mcycleAccess   = CsrAccess.NONE,
+            minstretAccess = CsrAccess.NONE,
+            ecallGen       = false,
+            wfiGenAsWait         = false,
+            ucycleAccess   = CsrAccess.NONE
+          )
+        ),
+        new YamlPlugin("cpu0.yaml")
+      )
+    )
+    config
+  }
+}
+
+
+
+class Corenc(config: CorencConfig) extends Component{
+
+  //Legacy constructor
+  def this(axiFrequency: HertzNumber) {
+    this(CorencConfig.default.copy(axiFrequency = axiFrequency))
+  }
+
+  import config._
+  val debug = true
+  val interruptCount = 4
+
+  val io = new Bundle{
+    //Clocks / reset
+    val asyncReset = in Bool
+    val axiClk     = in Bool
+
+    //Main components IO
+    val jtag       = slave(Jtag())
+
+    //Peripherals IO
+    val gpioA         = master(TriStateArray(32 bits))
+    val uart          = master(Uart())
+    val timerExternal = in(PinsecTimerCtrlExternal())
+    val coreInterrupt = in Bool
+  }
+
+  val resetCtrlClockDomain = ClockDomain(
+    clock = io.axiClk,
+    config = ClockDomainConfig(
+      resetKind = BOOT
+    )
+  )
+
+  val resetCtrl = new ClockingArea(resetCtrlClockDomain) {
+    val systemResetUnbuffered  = False
+
+    //Implement an counter to keep the reset axiResetOrder high 64 cycles
+    // Also this counter will automaticly do a reset when the system boot.
+    val systemResetCounter = Reg(UInt(6 bits)) init(0)
+    when(systemResetCounter =/= U(systemResetCounter.range -> true)){
+      systemResetCounter := systemResetCounter + 1
+      systemResetUnbuffered := True
+    }
+    when(BufferCC(io.asyncReset)){
+      systemResetCounter := 0
+    }
+
+    //Create all reset used later in the design
+    val systemReset  = RegNext(systemResetUnbuffered)
+    val axiReset     = RegNext(systemResetUnbuffered)
+  }
+
+  val axiClockDomain = ClockDomain(
+    clock = io.axiClk,
+    reset = resetCtrl.axiReset,
+    frequency = FixedFrequency(axiFrequency)
+  )
+
+  val debugClockDomain = ClockDomain(
+    clock = io.axiClk,
+    reset = resetCtrl.systemReset,
+    frequency = FixedFrequency(axiFrequency)
+  )
+
+  val axi = new ClockingArea(axiClockDomain) {
+    val ram = Axi4SharedOnChipRam(
+      dataWidth = 32,
+      byteCount = onChipRamSize,
+      idWidth = 4
+    )
+
+    val apbBridge = Axi4SharedToApb3Bridge(
+      addressWidth = 20,
+      dataWidth    = 32,
+      idWidth      = 4
+    )
+
+    val gpioACtrl = Apb3Gpio(
+      gpioWidth = 32,
+      withReadSync = true
+    )
+    val timerCtrl = PinsecTimerCtrl()
+
+    val uartCtrl = Apb3UartCtrl(uartCtrlConfig)
+
+    val core = new Area{
+      val config = VexRiscvConfig(
+        plugins = cpuPlugins += new DebugPlugin(debugClockDomain)
+      )
+
+      val cpu = new VexRiscv(config)
+      var iBus : Axi4ReadOnly = null
+      var dBus : Axi4Shared = null
+      for(plugin <- config.plugins) plugin match{
+        case plugin : IBusSimplePlugin => iBus = plugin.iBus.toAxi4ReadOnly()
+        case plugin : IBusCachedPlugin => iBus = plugin.iBus.toAxi4ReadOnly()
+        case plugin : DBusSimplePlugin => dBus = plugin.dBus.toAxi4Shared()
+        case plugin : DBusCachedPlugin => dBus = plugin.dBus.toAxi4Shared(true)
+        case plugin : CsrPlugin        => {
+          plugin.externalInterrupt := BufferCC(io.coreInterrupt)
+          plugin.timerInterrupt := timerCtrl.io.interrupt
+        }
+        case plugin : DebugPlugin      => debugClockDomain{
+          resetCtrl.axiReset setWhen(RegNext(plugin.io.resetOut))
+          io.jtag <> plugin.io.bus.fromJtag()
+        }
+        case _ =>
+      }
+    }
+    val axiCrossbar = Axi4CrossbarFactory()
+
+    axiCrossbar.addSlaves(
+      ram.io.axi       -> (0x80000000L,   onChipRamSize),
+      apbBridge.io.axi -> (0xF0000000L,   1 MB)
+    )
+
+    axiCrossbar.addConnections(
+      core.iBus       -> List(ram.io.axi),
+      core.dBus       -> List(ram.io.axi, apbBridge.io.axi)
+    )
+
+    axiCrossbar.addPipelining(apbBridge.io.axi)((crossbar,bridge) => {
+      crossbar.sharedCmd.halfPipe() >> bridge.sharedCmd
+      crossbar.writeData.halfPipe() >> bridge.writeData
+      crossbar.writeRsp             << bridge.writeRsp
+      crossbar.readRsp              << bridge.readRsp
+    })
+
+    axiCrossbar.addPipelining(ram.io.axi)((crossbar,ctrl) => {
+      crossbar.sharedCmd.halfPipe()  >>  ctrl.sharedCmd
+      crossbar.writeData            >/-> ctrl.writeData
+      crossbar.writeRsp              <<  ctrl.writeRsp
+      crossbar.readRsp               <<  ctrl.readRsp
+    })
+
+    axiCrossbar.addPipelining(core.dBus)((cpu,crossbar) => {
+      cpu.sharedCmd             >>  crossbar.sharedCmd
+      cpu.writeData             >>  crossbar.writeData
+      cpu.writeRsp              <<  crossbar.writeRsp
+      cpu.readRsp               <-< crossbar.readRsp //Data cache directly use read responses without buffering, so pipeline it for FMax
+    })
+
+    axiCrossbar.build()
+
+    val apbDecoder = Apb3Decoder(
+      master = apbBridge.io.apb,
+      slaves = List(
+        gpioACtrl.io.apb -> (0x00000, 4 kB),
+        uartCtrl.io.apb  -> (0x10000, 4 kB),
+        timerCtrl.io.apb -> (0x20000, 4 kB)
+      )
+    )
+  }
+
+  io.gpioA          <> axi.gpioACtrl.io.gpio
+  io.timerExternal  <> axi.timerCtrl.io.external
+  io.uart           <> axi.uartCtrl.io.uart
+}
+
+//DE1-SoC
+object Corenc{
+  def main(args: Array[String]) {
+    val config = SpinalConfig()
+    config.generateVerilog({
+      val toplevel = new Corenc(CorencConfig.default)
+      HexTools.initRam(toplevel.axi.ram.ram, "software/sbl/build/sbl.hex", 0x80000000l)
+      toplevel
+    })
+  }
+}
---
+++ src/test/cpp/corenc/main.cpp
@@ -0,0 +1,122 @@
+#include "VCorenc.h"
+#include "VCorenc_Corenc.h"
+#ifdef REF
+#include "VCorenc_RiscvCore.h"
+#endif
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <stdint.h>
+#include <cstring>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+#include <time.h>
+#include <unistd.h>
+
+#include "VCorenc_VexRiscv.h"
+
+
+#include "../common/framework.h"
+#include "../common/jtag.h"
+#include "../common/uart.h"
+
+class VexRiscvTracer : public SimElement{
+public:
+	VCorenc_VexRiscv *cpu;
+	ofstream instructionTraces;
+	ofstream regTraces;
+
+	VexRiscvTracer(VCorenc_VexRiscv *cpu){
+		this->cpu = cpu;
+#ifdef TRACE_INSTRUCTION
+	instructionTraces.open ("instructionTrace.log");
+#endif
+#ifdef TRACE_REG
+	regTraces.open ("regTraces.log");
+#endif
+	}
+
+
+
+	virtual void preCycle(){
+#ifdef TRACE_INSTRUCTION
+		if(cpu->writeBack_arbitration_isFiring){
+			instructionTraces <<  hex << setw(8) <<  cpu->writeBack_INSTRUCTION << endl;
+		}
+#endif
+#ifdef TRACE_REG
+		if(cpu->writeBack_RegFilePlugin_regFileWrite_valid == 1 && cpu->writeBack_RegFilePlugin_regFileWrite_payload_address != 0){
+			regTraces << " PC " << hex << setw(8) <<  cpu->writeBack_PC << " : reg[" << dec << setw(2) << (uint32_t)cpu->writeBack_RegFilePlugin_regFileWrite_payload_address << "] = " << hex << setw(8) << cpu->writeBack_RegFilePlugin_regFileWrite_payload_data << endl;
+		}
+
+#endif
+	}
+};
+
+
+
+
+#include <SDL2/SDL.h>
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+
+class CorencWorkspace : public Workspace<VCorenc>{
+public:
+	CorencWorkspace() : Workspace("Corenc"){
+		ClockDomain *axiClk = new ClockDomain(&top->io_axiClk,NULL,83333,300000);
+		AsyncReset *asyncReset = new AsyncReset(&top->io_asyncReset,50000);
+		Jtag *jtag = new Jtag(&top->io_jtag_tms,&top->io_jtag_tdi,&top->io_jtag_tdo,&top->io_jtag_tck,80000);
+		UartRx *uartRx = new UartRx(&top->io_uart_txd,1.0e12/115200);
+		timeProcesses.push_back(axiClk);
+		timeProcesses.push_back(asyncReset);
+		timeProcesses.push_back(jtag);
+		timeProcesses.push_back(uartRx);
+
+		axiClk->add(new VexRiscvTracer(top->Corenc->axi_core_cpu));
+
+		top->io_coreInterrupt = 0;
+	}
+
+};
+
+
+struct timespec timer_start(){
+    struct timespec start_time;
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start_time);
+    return start_time;
+}
+
+long timer_end(struct timespec start_time){
+    struct timespec end_time;
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end_time);
+    uint64_t diffInNanos = end_time.tv_sec*1e9 + end_time.tv_nsec -  start_time.tv_sec*1e9 - start_time.tv_nsec;
+    return diffInNanos;
+}
+
+
+
+int main(int argc, char **argv, char **env) {
+
+	Verilated::randReset(2);
+	Verilated::commandArgs(argc, argv);
+
+	printf("BOOT\n");
+	timespec startedAt = timer_start();
+
+	CorencWorkspace().run(1e9);
+
+	uint64_t duration = timer_end(startedAt);
+	cout << endl << "****************************************************************" << endl;
+	cout << "Had simulate " << workspaceCycles << " clock cycles in " << duration*1e-9 << " s (" << workspaceCycles / (duration*1e-9) << " Khz)" << endl;
+	cout << "****************************************************************" << endl << endl;
+
+
+	exit(0);
+}
